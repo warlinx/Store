c6e274c111d62a096af661d7f38cec52c2045ce1
warlinux <warlinux@yandex.ru>
2021-07-15 22:40:03 +0300

    test commit

---
+++ src/com/company/collect/Cat.java
@@ -0,0 +1,26 @@
+package com.company.collect;
+
+import java.util.ArrayList;
+
+public class Cat {
+    public Cat(String пушок) {
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Cat> cats = new ArrayList<>();
+        Cat thomas = new Cat("Томас");
+        Cat behemoth = new Cat("Бегемот");
+        Cat philipp = new Cat("Филипп Маркович");
+        Cat pushok = new Cat("Пушок");
+
+        cats.add(thomas);
+        cats.add(behemoth);
+        cats.add(philipp);
+        cats.add(pushok);
+
+        int thomasIndex = cats.indexOf(thomas);
+        System.out.println(thomasIndex);
+    }
+
+}
